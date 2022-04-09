32503ebb845a70a81fd0bbcb1bc5000c44d52fa7
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-04-09 11:49:59 -0400

    add api credentials

--- App.js
+++ App.js
@@ -3,12 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import firebase from 'firebase' 
 
-if(firebase.apps.length === 0){
-  firebase.initializeApp(firebaseConfig)
-}
-
 const firebase = require('firebase');
 require('firebase/firestore');
+
+if(firebase.apps.length){
+  firebase.initializeApp({
+    // insert firebase credentials here
+    apiKey: "AIzaSyDYPLerKjgHCpYQIzvsrxHWoNMfY0kWY3E",
+    authDomain: "test-a0508.firebaseapp.com",
+    projectId: "test-a0508",
+    storageBucket: "test-a0508.appspot.com",  
+    messagingSenderId: "371451181680", 
+    appId: "1:371451181680:web:3c5ecc81bb4d65bbd592b5", 
+    measurementId: "G-X93VVSDWPS"
+  });
+}
+
+this.referenceShoppingLists = firebase.firestore().collection('shoppinglists');
 
 export default function App() {
   return (
